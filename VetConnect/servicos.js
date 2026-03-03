03f6e7587128d4b972266eb3ba16c3a2142a7bd3
AdrianoWild <adrianowilddeavila@gmail.com>
2026-03-03 18:45:40 -0300

    Atualizações do meu projeto

---
+++ VetConnect/servicos.js
@@ -0,0 +1,25 @@
+function clicarconsulta()
+{
+    document.getElementById('consulta').style.backgroundColor = "darkgray";
+    document.getElementById('consulta').style.color = "white";
+}
+function clicarexame()
+{
+    document.getElementById('exame').style.backgroundColor = "darkgray";
+    document.getElementById('exame').style.color = "white";
+}
+function clicarbanho()
+{
+    document.getElementById('banho').style.backgroundColor = "darkgray";
+    document.getElementById('banho').style.color = "white";
+}
+function clicarvacina()
+{
+    document.getElementById('vacina').style.backgroundColor = "darkgray";
+    document.getElementById('vacina').style.color = "white";
+}
+function clicarcirurgia()
+{
+    document.getElementById('cirurgia').style.backgroundColor = "darkgray";
+    document.getElementById('cirurgia').style.color = "white";
+}
